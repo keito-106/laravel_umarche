c3aee5e573d278f9d696fb531997cc6d056c3043
Keito Kunimoto <kate1@KateMacBook-Pro.local>
2025-06-05 19:21:35 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             OwnerSeeder::class,
             ShopSeeder::class,
             ImageSeeder::class,
+            CategorySeeder::class,
             // Add other seeders here as needed
         ]);
     }
