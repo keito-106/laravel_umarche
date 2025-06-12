3ddf2ac915e154109a5d84d4c803a882f817cf06
Keito Kunimoto <kate1@KateMacBook-Pro.local>
2025-06-12 12:38:10 +0900

    ユーザーの一覧画面初期設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             // ProductSeeder::class,
             // StockSeeder::class,
+            UserSeeder::class,
             // Add other seeders here as needed
         ]);
     }
