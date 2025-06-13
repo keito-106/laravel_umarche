cda749e048d904dd09de0d682fcdf0fd43357c58
Keito Kunimoto <kate1@KateMacBook-Pro.local>
2025-06-13 16:32:36 +0900

    カート追加機能実装

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -69,23 +69,27 @@
                             <div class="flex items-center">
                                 <span class="title-font font-medium text-2xl text-gray-900"><span
                                         class="text-gray-700">¥</span>{{ number_format($product->price) }}
-                                    <spav class="text-sm text-gray-700">
+                                    <span class="text-sm text-gray-700">
                                         (税込)
-                                    </spav>
+                                    </span>
                                 </span>
-                                <div class="flex items-center ml-4">
-                                    <span class="mr-3">数量</span>
-                                    <div class="relative mr-4">
-                                        <select name="quantity"
-                                            class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                                            @for ($i = 1; $i <= $quantity; $i++)
-                                                <option value="{{ $i }}">{{ $i }}</option>
-                                            @endfor
-                                        </select>
+                                <form class="flex" method="post" action="{{ route('user.cart.add') }}">
+                                    @csrf
+                                    <div class="flex items-center ml-4">
+                                        <span class="mr-3">数量</span>
+                                        <div class="relative mr-4">
+                                            <select name="quantity"
+                                                class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                                                @for ($i = 1; $i <= $quantity; $i++)
+                                                    <option value="{{ $i }}">{{ $i }}</option>
+                                                @endfor
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <button
-                                    class="flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                                    <button
+                                        class="flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                </form>
                             </div>
                         </div>
                     </div>
