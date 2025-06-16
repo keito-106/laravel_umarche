c91ec29408b69a1d03a2356233bd35336d3d721f
Keito Kunimoto <kate1@KateMacBook-Pro.local>
2025-06-16 16:13:42 +0900

    stripeキャンセル処理追加

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -101,8 +101,8 @@ class CartController extends Controller
             'line_items' => [$lineItems],
             'mode' => 'payment',
 
-            'success_url' => route('user.cart.index'),
-            'cancel_url' => route('user.cart.index'),
+            'success_url' => route('user.cart.success'),
+            'cancel_url' => route('user.cart.cancel'),
         ]);
 
         $publicKey = env('STRIPE_PUBLIC_KEY');
@@ -115,8 +115,26 @@ class CartController extends Controller
     {
         Cart::where('user_id', Auth::id())->delete();
 
-        return redirect()->route('user.items.index')
-            ->with('success', '決済が完了しました。');
-    }
+        return redirect()->route('user.items.index')->with([
+            'success' => '決済が完了しました。'
+        ]);
+     }
+
+     public function cancel()
+     {
+        $user = User::findOrFail(Auth::id());
+
+        foreach($user->products as $product){
+            Stock::create([
+                    'product_id' => $product->id,
+                    'type' => \Constant::PRODUCT_LIST['add'],
+                    'quantity' => $product->pivot->quantity,
+                ]);
+        }
+
+        return redirect()->route('user.cart.index')->with([
+            'error' => '決済がキャンセルされました。'
+        ]);
+     }
 
 }
